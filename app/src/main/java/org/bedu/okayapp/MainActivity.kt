f2092511a86b4786238128c21372a810786447f2
LauAlvarezG <lora_ag@yahoo.com.mx>
2021-09-02 03:30:00 -0500

    Hola, equipo, hice algunos cambios en la main Activity y puse un ejemplo. Aún está por decidirse nombre y logo, pero para ir probando.

--- app/src/main/java/org/bedu/okayapp/MainActivity.kt
+++ app/src/main/java/org/bedu/okayapp/MainActivity.kt
@@ -3,6 +3,7 @@ package org.bedu.okayapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Button
 import android.widget.ImageView
 
@@ -16,13 +17,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        main_img_logo= findViewById(R.id.main_img_logo)
-        main_img_apptitle= findViewById(R.id.main_img_apptitle)
-        btnprueba= findViewById(R.id.btnprueba)
+        //Para mejorar apariencia quitando la barra de arriba
+        supportActionBar?.hide()
 
-        btnprueba.setOnClickListener {
+        //Agregar Handler para que automáticamente después de 2.5 seg pase a la act. Menu
+      Handler().postDelayed({
+          val intent = Intent(this@MainActivity,Menu::class.java)
+          startActivity(intent)
+          finish()
+      }, 2500)
+
+
+        //Comento esta parte para cargar el intro a partir del svg
+
+        /*main_img_logo= findViewById(R.id.main_img_logo)
+        main_img_apptitle= findViewById(R.id.main_img_apptitle)
+        btnprueba= findViewById(R.id.btnprueba)*/
+
+        /*btnprueba.setOnClickListener {
             val activity_menu: Intent = Intent(this,Menu::class.java )
             startActivity(activity_menu)
-        }
+        }*/
     }
 }
