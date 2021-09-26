bb40441dfb24cb8e064b8ec76e7077e903d8d359
Jess-CR <jessica.cordova.rangel@uabc.edu.mx>
2021-09-26 16:50:17 -0700

    .

---
+++ app/src/main/java/org/bedu/okayapp/Inicio/Profile.kt
@@ -0,0 +1,12 @@
+package org.bedu.okayapp.Inicio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.bedu.okayapp.R
+
+class Profile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
