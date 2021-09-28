b5047c7b8fee05efcfb24df9fdfc67c37fd384e8
Jess-CR <jessica.cordova.rangel@uabc.edu.mx>
2021-09-28 16:59:01 -0700

    radiobutton
    Se añaden.

--- app/src/main/java/org/bedu/okayapp/Inicio/SignUp.kt
+++ app/src/main/java/org/bedu/okayapp/Inicio/SignUp.kt
@@ -1,11 +1,11 @@
 package org.bedu.okayapp.Inicio
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
-import android.widget.NumberPicker
-import android.widget.TextView
+import android.widget.*
 import org.bedu.okayapp.R
+import org.bedu.okayapp.Temas.categories
 
 class SignUp : AppCompatActivity() {
 
@@ -14,11 +14,13 @@ class SignUp : AppCompatActivity() {
     private lateinit var sign_up_editText_email:EditText
     private lateinit var sign_up_editText_password:EditText
     private lateinit var sign_up_editText_passwordConfirmation:EditText
-    private lateinit var sign_up_editText_profession:EditText
+    private lateinit var sign_up_RadioGroup:RadioGroup
+    private lateinit var signup_radioButton_student: RadioButton
+    private lateinit var signup_radioButton_professor: RadioButton
     private lateinit var sign_up_editText_date:EditText
+    private lateinit var sign_up_btn_continuar:Button
 
-    //Intenté poner el dropdown menu para seleccionar Estudiante o profesor
-    //private val binding get() = binding!!
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,8 +31,10 @@ class SignUp : AppCompatActivity() {
         sign_up_editText_email=findViewById(R.id.sign_up_editText_email)
         sign_up_editText_password=findViewById(R.id.sign_up_editText_password)
         sign_up_editText_passwordConfirmation=findViewById(R.id.sign_up_editText_passwordConfirmation)
-        sign_up_editText_profession=findViewById(R.id.sign_up_editText_profession)
+        signup_radioButton_professor=findViewById(R.id.signup_radioButton_professor)
+        signup_radioButton_student=findViewById(R.id.signup_radioButton_student)
         sign_up_editText_date=findViewById(R.id.sign_up_editText_date)
+        sign_up_RadioGroup=findViewById(R.id.sign_up_RadioGroup)
 
         sign_up_editText_date.setOnClickListener{showDatePicker()}
 
@@ -42,6 +46,12 @@ class SignUp : AppCompatActivity() {
 
         return binding.root*/
 
+//        sign_up_RadioGroup.setOnCheckedChangeListener{sign_up_Radiogroup, i ->
+  //          var rb = findViewById<RadioGroup>(i)
+    //    }
+
+
+        activity_signUp()
     }
 
     private fun showDatePicker() {
@@ -51,5 +61,12 @@ class SignUp : AppCompatActivity() {
 
     fun onDateSelected(day:Int, month:Int, year:Int){
         sign_up_editText_date.setText("$day / $month / $year")
+    }
+
+    fun activity_signUp(){
+        sign_up_btn_continuar.setOnClickListener {
+            val ventanaLogIn: Intent = Intent(this, LogIn::class.java)
+            startActivity(ventanaLogIn)
+        }
     }
 }
