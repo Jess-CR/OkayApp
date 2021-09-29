8b4d1337bbb89042f1324d0bf7a3105b2bc916bf
Jess-CR <jessica.cordova.rangel@uabc.edu.mx>
2021-09-28 18:52:52 -0700

    Intent profile
    Se liga el botón Perfil de la pantalla Progreso a la pantalla perfil en la que se pide autorización de utilizar la cámara

--- app/src/main/java/org/bedu/okayapp/Progreso/Progress.kt
+++ app/src/main/java/org/bedu/okayapp/Progreso/Progress.kt
@@ -5,19 +5,27 @@ import org.bedu.okayapp.Trivia.BaseQuestion
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import org.bedu.okayapp.Inicio.LogIn
+import org.bedu.okayapp.Inicio.Profile
+import org.bedu.okayapp.R
 import org.bedu.okayapp.databinding.ActivityProgressBinding
 
 class Progress : AppCompatActivity() {
+
     private lateinit var binding: ActivityProgressBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         binding = ActivityProgressBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val progressBar = ProgressBarAnimation(
             binding.progressPogressBar,
             binding.progressPogressStatus,
-            binding.progressTextPercentage
+            binding.progressTextPercentage,
         )
 
         progressBar.setProgress(75)
@@ -29,6 +37,15 @@ class Progress : AppCompatActivity() {
         }
         //hasta aqui
 
+        activity_profile()
+    }
 
+    //Jessica --> con esto se liga a la pantalla de profile
+    fun activity_profile(){
+        binding.progressBtnProfile.setOnClickListener {
+            val ventanaProfile: Intent = Intent(this, Profile::class.java)
+            startActivity(ventanaProfile)
+            }
     }
 }
+
